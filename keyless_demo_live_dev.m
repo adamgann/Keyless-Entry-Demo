2390858aeb18afd8ccb9469f393b72b0a4a611fb
triskelion <triskelion@triskelion.(none)>
2014-10-13 16:00:37 -0400

    Added All

---
+++ keyless_demo_live_dev.m
@@ -0,0 +1,216 @@
+%
+%
+%
+
+vaporize
+
+
+%% Parameters
+
+threshold = 0.2;
+pkt_len = 6000;
+pulse_len = 22;
+window_len = 10;
+
+buffer_len = 50000;
+
+
+rx_pkts = zeros(35,35);
+pkt_counter = 0;
+
+
+
+%% Data
+
+
+%keyless_mag = read_float_binary('data/keyless_mag.dat');
+%fi = fopen('data/keyless_mag_fifo','rb');
+
+
+
+handFig = figure(1);
+set(gcf,'Color', 'white')
+set(handFig, 'Position', [0 0 1600 300])
+clf
+set(gca,'Color','white');
+set(gca,'XColor','white');
+set(gca,'YColor','white');
+
+
+good_str = 'Transmission Received';
+disp_counter = 0;
+tic
+
+
+[imgHand,map] = imread('data/logo.gif','gif');
+[imgHand,map] = imresize(imgHand,map,0.5);
+hold on
+imshow(imgHand,map);
+truesize
+
+break
+%% Energy Detection
+
+in_buff = fread(fi, buffer_len, 'float');
+raw = in_buff;
+
+while(1)
+
+s=toc;
+if (s>3)
+  clf
+  set(gca,'Color','white');
+  set(gca,'XColor','white');
+  set(gca,'YColor','white');
+  clear goodHand
+  clear dataHand
+  
+end
+
+drawnow
+  
+eng_mat = vec2mat(raw,10);
+x_mag = abs(eng_mat).^2;
+x_sum = sum(x_mag,2);
+
+b = x_sum(2:end);
+a = x_sum(1:end-1);
+
+x_diff = b./a;
+x_norm = x_diff./max(x_diff);
+
+
+
+x_ind = find(x_norm>threshold);
+if (isempty(x_ind))
+    x_ind = 1;
+end
+
+start_ind = x_ind(1)*window_len;
+
+
+if (start_ind+pkt_len)>length(raw)
+    in_buff = fread(fi, buffer_len, 'float');
+    if (isempty(in_buff))
+        %display('Buffer Empty')
+        continue
+    end
+    raw = cat(1,raw,in_buff);
+end
+
+
+
+vec = zeros(size(raw));
+vec(start_ind) = 1;
+
+x_pkt = raw(start_ind:start_ind+pkt_len);
+raw = raw(start_ind+pkt_len+1:end);
+
+
+%% Filter The Signal
+
+n=2;
+x_filt = filter(ones(1,n)/n,1,x_pkt);
+
+
+x_dec = x_filt;
+x_dec(x_dec>0.3) = 1;
+x_dec(x_dec~=1) = 0;
+
+
+
+x_ind = find(x_dec);
+
+if (isempty(x_ind))
+    continue;
+end
+
+start_ind = x_ind(1);
+
+x_dec = x_dec(start_ind:end);
+
+
+
+
+
+
+%% Decode The Bits
+counter =0;
+bit_ind = 1;
+for ii=2:length(x_dec)-1
+    if (x_dec(ii)~=x_dec(ii-1)) % Transition
+        counter = 0;
+    else
+        counter=counter+1;
+        if (counter>16)
+            counter=0;
+            bit(bit_ind) = x_dec(ii);
+            bit_ind=bit_ind+1;
+        end
+    end
+end
+
+%bit = bit(1:2:end);
+bit_group = vec2mat(bit,8);
+byte = bi2de(bit_group)';
+
+
+%% Decode The Packet
+known_sync = 85*ones(1,13);
+if (length(byte)<14)
+    continue
+end
+sync = byte(1:13);
+payload = byte(14:end);
+
+pkt_good = false;
+if (isequal(sync,known_sync))
+    display('Received Pkt')
+    pkt_good=true;
+end
+
+
+
+if (pkt_good)
+    disp_counter = 4
+    goodHand = text(0,0.5,good_str,'fontsize',16,'color','r');
+end 
+drawnow
+
+if (~pkt_good)
+   continue
+end
+
+
+dec2hex(payload)
+pkt_counter=pkt_counter+1;
+rx_payload(pkt_counter,1:length(payload)) = payload;
+   
+
+%% Plot the results
+text_str = [];
+for ii=1:length(byte)
+    text_str = strcat(text_str, sprintf(' %d  ',byte(ii)));
+end
+
+preamble_str = [];
+for ii=1:length(sync)
+    preamble_str = strcat(preamble_str, sprintf(' %d  ',sync(ii)));
+end
+
+if (exist('dataHand'))
+    delete(dataHand)
+    clear dataHand
+end
+dataHand = text(0,0.1,text_str,'fontsize',16,'color','k');
+
+text(0,0,' |----------------------Sync----------------------|',...
+    'fontsize',16,'color','b')
+text(0.25,0,' |--------------------------------------------Button Code ---------------------------------------------------------------------|',...
+    'fontsize',16,'color','b')
+
+drawnow
+tic
+
+
+end
---
+++ keyless_live.py
@@ -0,0 +1,239 @@
+#!/usr/bin/env python
+##################################################
+# Gnuradio Python Flow Graph
+# Title: Keyless Live
+# Generated: Mon Oct 13 09:16:56 2014
+##################################################
+
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from gnuradio.wxgui import forms
+from gnuradio.wxgui import scopesink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import osmosdr
+import wx
+
+class keyless_live(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Keyless Live")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.vol = vol = 1
+        self.samp_rate = samp_rate = 2e6
+        self.gain = gain = 50
+        self.fine_freq = fine_freq = 0
+        self.cutoff = cutoff = 100e3
+        self.alpha = alpha = 0.5
+
+        ##################################################
+        # Blocks
+        ##################################################
+        _gain_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._gain_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_gain_sizer,
+        	value=self.gain,
+        	callback=self.set_gain,
+        	label='gain',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._gain_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_gain_sizer,
+        	value=self.gain,
+        	callback=self.set_gain,
+        	minimum=0,
+        	maximum=100,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_gain_sizer)
+        _fine_freq_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._fine_freq_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_fine_freq_sizer,
+        	value=self.fine_freq,
+        	callback=self.set_fine_freq,
+        	label='fine_freq',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._fine_freq_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_fine_freq_sizer,
+        	value=self.fine_freq,
+        	callback=self.set_fine_freq,
+        	minimum=-500e3,
+        	maximum=500e3,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_fine_freq_sizer)
+        self.wxgui_scopesink2_0 = scopesink2.scope_sink_f(
+        	self.GetWin(),
+        	title="Scope Plot",
+        	sample_rate=50e3,
+        	v_scale=1,
+        	v_offset=0,
+        	t_scale=1e-3,
+        	ac_couple=False,
+        	xy_mode=False,
+        	num_inputs=1,
+        	trig_mode=wxgui.TRIG_MODE_AUTO,
+        	y_axis_label="Counts",
+        )
+        self.Add(self.wxgui_scopesink2_0.win)
+        _vol_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._vol_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_vol_sizer,
+        	value=self.vol,
+        	callback=self.set_vol,
+        	label='vol',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._vol_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_vol_sizer,
+        	value=self.vol,
+        	callback=self.set_vol,
+        	minimum=1,
+        	maximum=25,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_vol_sizer)
+        self.rtlsdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + "" )
+        self.rtlsdr_source_0.set_sample_rate(samp_rate)
+        self.rtlsdr_source_0.set_center_freq(315e6+fine_freq, 0)
+        self.rtlsdr_source_0.set_freq_corr(0, 0)
+        self.rtlsdr_source_0.set_dc_offset_mode(2, 0)
+        self.rtlsdr_source_0.set_iq_balance_mode(2, 0)
+        self.rtlsdr_source_0.set_gain_mode(False, 0)
+        self.rtlsdr_source_0.set_gain(gain, 0)
+        self.rtlsdr_source_0.set_if_gain(60, 0)
+        self.rtlsdr_source_0.set_bb_gain(60, 0)
+        self.rtlsdr_source_0.set_antenna("", 0)
+        self.rtlsdr_source_0.set_bandwidth(0, 0)
+          
+        self.low_pass_filter_0 = filter.fir_filter_ccf(40, firdes.low_pass(
+        	1, samp_rate, cutoff, 2*cutoff, firdes.WIN_HAMMING, 6.76))
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_float*1, "/home/triskelion/Documents/Demo/Keyless/data/keyless_mag_fifo", False)
+        self.blocks_file_sink_0.set_unbuffered(True)
+        self.blocks_complex_to_mag_0 = blocks.complex_to_mag(1)
+        _alpha_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._alpha_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_alpha_sizer,
+        	value=self.alpha,
+        	callback=self.set_alpha,
+        	label='alpha',
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._alpha_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_alpha_sizer,
+        	value=self.alpha,
+        	callback=self.set_alpha,
+        	minimum=0.1,
+        	maximum=1,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_alpha_sizer)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_complex_to_mag_0, 0), (self.wxgui_scopesink2_0, 0))
+        self.connect((self.rtlsdr_source_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.blocks_complex_to_mag_0, 0))
+        self.connect((self.blocks_complex_to_mag_0, 0), (self.blocks_file_sink_0, 0))
+
+
+
+    def get_vol(self):
+        return self.vol
+
+    def set_vol(self, vol):
+        self.vol = vol
+        self._vol_slider.set_value(self.vol)
+        self._vol_text_box.set_value(self.vol)
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.rtlsdr_source_0.set_sample_rate(self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.cutoff, 2*self.cutoff, firdes.WIN_HAMMING, 6.76))
+
+    def get_gain(self):
+        return self.gain
+
+    def set_gain(self, gain):
+        self.gain = gain
+        self._gain_slider.set_value(self.gain)
+        self._gain_text_box.set_value(self.gain)
+        self.rtlsdr_source_0.set_gain(self.gain, 0)
+
+    def get_fine_freq(self):
+        return self.fine_freq
+
+    def set_fine_freq(self, fine_freq):
+        self.fine_freq = fine_freq
+        self._fine_freq_slider.set_value(self.fine_freq)
+        self._fine_freq_text_box.set_value(self.fine_freq)
+        self.rtlsdr_source_0.set_center_freq(315e6+self.fine_freq, 0)
+
+    def get_cutoff(self):
+        return self.cutoff
+
+    def set_cutoff(self, cutoff):
+        self.cutoff = cutoff
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.cutoff, 2*self.cutoff, firdes.WIN_HAMMING, 6.76))
+
+    def get_alpha(self):
+        return self.alpha
+
+    def set_alpha(self, alpha):
+        self.alpha = alpha
+        self._alpha_slider.set_value(self.alpha)
+        self._alpha_text_box.set_value(self.alpha)
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    (options, args) = parser.parse_args()
+    tb = keyless_live()
+    tb.Start(True)
+    tb.Wait()
